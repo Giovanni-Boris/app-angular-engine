94a2c0a42718ee4429c3b17d5f2ff77ad36a3cd9
EAncoHuaracha <eancohu@unsa.edu.pe>
2024-11-24 12:46:45 -0500

    Adding guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { InventoryRoutes } from './core/config/routes.constants';
 import { LoginComponent } from './core/auth/login/login.component';
 import { FeaturesComponent } from './features/features.component';
+import { authGuard } from './core/auth/auth.guard';
 
 export const routes: Routes = [
   {
@@ -12,7 +13,7 @@ export const routes: Routes = [
     path: InventoryRoutes.BASE,
     component: FeaturesComponent,
     loadChildren: () => import('./features/inventory/inventory.routes').then(m => m.inventoryRoutes),
-    canActivate: []
+    canActivate: [authGuard]
   },
   {
     path: "**",
