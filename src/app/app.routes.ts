3b8d641fcbc273558baa00ffbcc0975ba7f2cb4d
EAncoHuaracha <eancohu@unsa.edu.pe>
2024-11-24 10:59:00 -0500

    Update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { InventoryRoutes } from './core/config/routes.constants';
+import { LoginComponent } from './core/auth/login/login.component';
+import { FeaturesComponent } from './features/features.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: InventoryRoutes.BASE,
+    component: FeaturesComponent,
+    loadChildren: () => import('./features/inventory/inventory.routes').then(m => m.inventoryRoutes),
+    canActivate: []
+  },
+  {
+    path: "**",
+    redirectTo: "",
+    pathMatch: 'full'
+  }
+];
